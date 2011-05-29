5a66a7478c8316e852cc90ea804f480e3342956a
Jack Chu <kamuigt@gmail.com>
2011-05-29 17:55:11 -0400

    upgrade to rails 2.3.11, and set authlogic to version 2.1.6 since only 2.x.x supports rails 2.3.x

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.9' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.11' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -19,7 +19,7 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem "authlogic"
+  config.gem "authlogic", :version => "~> 2.1.6"
   config.gem "bcrypt-ruby", :lib => "bcrypt"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
